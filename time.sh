12b01edc8d142aa7d1f221130da8c84572fe5941
Matteo Franzil <matteofranzil@gmail.com>
2023-05-11 15:29:57 +0200

    Fixed pgrep for Linux

--- time.sh
+++ time.sh
@@ -1,14 +1,17 @@
 #!/bin/zsh
 
 while [ 0 = 0 ]; do
-    if pgrep gzip gunzip &>/dev/null; then
-        printf "%s - gzip gunzip are running [%s]\n" "$(date)" "$(pgrep gzip gunzip | xargs ps | grep -E "(gzip|gunzip)" )"
-        while pgrep gzip gunzip &>/dev/null; do
+    if pgrep "gzip|gunzip" &>/dev/null; then
+        printf "%s - gzip gunzip are running [%s]\n" "$(date)" "$(pgrep "gzip|gunzip" | xargs ps | grep -E "(gzip|gunzip)" )"
+        while pgrep "gzip|gunzip" &>/dev/null; do
             printf "%s - " "$(date)"
             if [[ "$PLATFORM" == "mac" ]]; then
-                top -l1 -s 0 | grep ^CPU | cut -f 1 -d % | rev | cut -f 1 -d ' ' | rev
+                echo
+                # top -l1 -s 0 | grep ^CPU | cut -f 1 -d % | rev | cut -f 1 -d ' ' | rev
             else
-                top -bn1 | awk '/Cpu/ { print $2}'
+                cpu=$(pgrep "gzip|gunzip" | xargs ps  -L -o pid,tid,psr,pcpu | tail -n 1 | rev | cut -f 1 -d " " | rev) 
+                printf "%s\n" "$cpu"
+                # top -bn1 | awk '/Cpu/ { print $2}'
             fi
         done
     else
